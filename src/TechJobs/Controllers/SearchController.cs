f0854ffdb1bd34e64c9e2da04770c17ae834c0ce
Mary Bellavia <sauerwein.mary@gmail.com>
2019-12-17 14:04:19 -0600

    finished commenting out my code

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -17,21 +17,29 @@ namespace TechJobs.Controllers
         {
             // making a new list of dictionarys for results of the search
             List<Dictionary<string, string>> jobsResults = new List<Dictionary<string, string>>();
+            // needed for the top portion of the search bar to work
+            ViewBag.columns = ListController.columnChoices;
+            // changed the title of the page to results bc why not
+            ViewBag.title = "Results";
 
-            // conditional statement to differentiate the need for find by column or find by value to create list of dictionaries with results
+            /* conditional statement to differentiate the need for find by column
+             * or find by value to create list of dictionaries for search results */
+
+            // if they pick a specific column to search by
             if (searchType != "all")
             {
                 jobsResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+                // creating my ViewBag for use in the cs.html
                 ViewBag.jobsResults = jobsResults;
             }
-            else
+            else // user wan'ts to search across all columns
             {
                 jobsResults = JobData.FindByValue(searchTerm);
+                // creating my ViewBag for use in the cs.html
                 ViewBag.jobsResults = jobsResults;
             }
 
-            ViewBag.columns = ListController.columnChoices;
-            ViewBag.title = "Results";
+            // returning the correct view / html template
             return View("Views/Search/Index.cshtml");
 
 
