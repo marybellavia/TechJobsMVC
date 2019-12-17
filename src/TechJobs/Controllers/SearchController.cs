c032045e051835c76488da19ae9def29f5d385cb
Mary Bellavia <sauerwein.mary@gmail.com>
2019-12-17 10:53:52 -0600

    created Results action and began processing search

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,7 +13,27 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
+        public IActionResult Result(string searchType, string searchTerm)
+        {
+            // making a new list of dictionarys for results of the search
+            List<Dictionary<string, string>> jobsResults = new List<Dictionary<string, string>>();
+
+            // conditional statement to differentiate the need for find by column or find by value to create list of dictionaries with results
+            if (searchType != "all")
+            {
+                jobsResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            else
+            {
+                jobsResults = JobData.FindByValue(searchTerm);
+            }
+
+            // so i've created this dictionary of results -- now WTF do I do with it
+
+            return View("Index");
+
+        }
+            // TODO #1 - Create a Results action method to process 
         // search request and display results
 
     }
